7e5586b2a07ba88c51bdda61cea9df360044f809
Martin Lippert <martin.lippert@broadcom.com>
2025-06-12 08:23:41 +0200

    update build actions to new targets

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,1 +1,1 @@
-./mvnw clean package -Pe435 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
+./mvnw clean package -Pe436 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5

76f6fd9bb266cfa4204f8e939459470c92a1843f
Martin Lippert <martin.lippert@broadcom.com>
2025-10-30 15:54:25 +0100

    added enable/disable logic for ai pref page

--- eclipse-language-servers/org.springframework.tooling.boot.ls/src/org/springframework/tooling/boot/ls/prefs/AiConfigPreferencePage.java
+++ eclipse-language-servers/org.springframework.tooling.boot.ls/src/org/springframework/tooling/boot/ls/prefs/AiConfigPreferencePage.java
@@ -13,6 +13,7 @@ package org.springframework.tooling.boot.ls.prefs;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
@@ -20,6 +21,9 @@ import org.springframework.tooling.boot.ls.BootLanguageServerPlugin;
 import org.springframework.tooling.boot.ls.Constants;
 
 public class AiConfigPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+	
+	private BooleanFieldEditor mcpEnabledEditor;
+	private StringFieldEditor mcpPortEditor;
 	
 	public AiConfigPreferencePage() {
 		super(GRID);
@@ -33,8 +37,32 @@ public class AiConfigPreferencePage extends FieldEditorPreferencePage implements
 	@Override
 	protected void createFieldEditors() {
 		Composite fieldEditorParent = getFieldEditorParent();
-		addField(new BooleanFieldEditor(Constants.PREF_AI_MCP_ENABLED, "Enable embedded MCP server (experimental)", fieldEditorParent));
-		addField(new StringFieldEditor(Constants.PREF_AI_MCP_PORT, "Port used by the embedded MCP server", fieldEditorParent));
+		
+		mcpEnabledEditor = new BooleanFieldEditor(Constants.PREF_AI_MCP_ENABLED, "Enable embedded MCP server (experimental)", fieldEditorParent);
+		addField(mcpEnabledEditor);
+		
+		mcpPortEditor = new StringFieldEditor(Constants.PREF_AI_MCP_PORT, "Port used by the embedded MCP server", fieldEditorParent);
+		addField(mcpPortEditor);
+		
+		// Initialize the enabled state of the port field
+		updateMcpPortFieldState();
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		super.propertyChange(event);
+		
+		// Update the MCP port field enabled state when the MCP enabled preference changes
+		if (event.getSource() == mcpEnabledEditor) {
+			updateMcpPortFieldState();
+		}
+	}
+	
+	private void updateMcpPortFieldState() {
+		if (mcpEnabledEditor != null && mcpPortEditor != null) {
+			boolean mcpEnabled = mcpEnabledEditor.getBooleanValue();
+			mcpPortEditor.setEnabled(mcpEnabled, getFieldEditorParent());
+		}
 	}
 
 }

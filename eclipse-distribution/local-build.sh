69e13671361c142dab370d2d0f2a3d44b7abf4bb
Martin Lippert <martin.lippert@broadcom.com>
2025-12-11 12:12:25 +0100

    update build targets and pipelines to new eclipse cycle

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,1 +1,1 @@
-./mvnw clean package -Pe437 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
+./mvnw clean package -Pe438 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5

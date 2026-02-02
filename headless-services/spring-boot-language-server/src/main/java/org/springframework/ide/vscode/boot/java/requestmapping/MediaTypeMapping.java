8672fdb889f164bcac5a500c25e8a77bce67e8cf
Martin Lippert <martin.lippert@broadcom.com>
2026-02-02 13:45:46 +0100

    updated media types mapping

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/requestmapping/MediaTypeMapping.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/requestmapping/MediaTypeMapping.java
@@ -17,28 +17,35 @@ public enum MediaTypeMapping {
 	
 	ALL("*/*"),
 	APPLICATION_ATOM_XML("application/atom+xml"),
+	APPLICATION_CBOR("application/cbor"),
 	APPLICATION_FORM_URLENCODED("application/x-www-form-urlencoded"),
+	APPLICATION_GRAPHQL_RESPONSE("application/graphql-response+json"),
 	APPLICATION_JSON("application/json"),
+	APPLICATION_NDJSON("application/x-ndjson"),
 	APPLICATION_JSON_UTF8("application/json;charset=UTF-8"),
 	APPLICATION_OCTET_STREAM("application/octet-stream"),
 	APPLICATION_PDF("application/pdf"),
 	APPLICATION_PROBLEM_JSON("application/problem+json"),
 	APPLICATION_PROBLEM_JSON_UTF8("application/problem+json;charset=UTF-8"),
 	APPLICATION_PROBLEM_XML("application/problem+xml"),
+	APPLICATION_PROTOBUF("application/x-protobuf"),
 	APPLICATION_RSS_XML("application/rss+xml"),
 	APPLICATION_STREAM_JSON("application/stream+json"),
 	APPLICATION_XHTML_XML("application/xhtml+xml"),
 	APPLICATION_XML("application/xml"),
+	APPLICATION_YAML("application/yaml"),
 	IMAGE_GIF("image/gif"),
 	IMAGE_JPEG("image/jpeg"),
 	IMAGE_PNG("image/png"),
 	MULTIPART_FORM_DATA("multipart/form-data"),
+	MULTIPART_MIXED("multipart/mixed"),
+	MULTIPART_RELATED("multipart/related"),
 	TEXT_EVENT_STREAM("text/event-stream"),
 	TEXT_HTML("text/html"),
 	TEXT_MARKDOWN("text/markdown"),
 	TEXT_PLAIN("text/plain"),
 	TEXT_XML("text/xml");
-
+	
 	private String mediaType;
 
 	private MediaTypeMapping(String mediaType) {
@@ -50,3 +57,5 @@ public enum MediaTypeMapping {
 	}
 
 }
+
+

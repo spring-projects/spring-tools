99ee6e0d72f9b924e1cbb5c83a3363acb9984e0f
BoykoAlex <alex.boyko@broadcom.com>
2026-02-19 17:35:31 -0800

    Quick fix for LoadBalancer and Feign client configuration
    Signed-off-by: BoykoAlex <alex.boyko@broadcom.com>

--- headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/java/reconcilers/test/AddConfigurationIfBeansPresentReconcilingLoadBalancerConfigCaseTest.java
+++ headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/java/reconcilers/test/AddConfigurationIfBeansPresentReconcilingLoadBalancerConfigCaseTest.java
@@ -11,9 +11,12 @@
 package org.springframework.ide.vscode.boot.java.reconcilers.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -34,6 +37,9 @@ import org.springframework.ide.vscode.boot.java.Boot2JavaProblemType;
 import org.springframework.ide.vscode.boot.java.utils.test.TestFileScanListener;
 import org.springframework.ide.vscode.commons.languageserver.java.JavaProjectFinder;
 import org.springframework.ide.vscode.commons.util.UriUtil;
+import org.springframework.ide.vscode.commons.util.text.LanguageId;
+import org.springframework.ide.vscode.languageserver.testharness.CodeAction;
+import org.springframework.ide.vscode.languageserver.testharness.Editor;
 import org.springframework.ide.vscode.project.harness.BootLanguageServerHarness;
 import org.springframework.ide.vscode.project.harness.ProjectsHarness;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -143,6 +149,65 @@ public class AddConfigurationIfBeansPresentReconcilingLoadBalancerConfigCaseTest
 		assertEquals(Boot2JavaProblemType.MISSING_CONFIGURATION_ANNOTATION.getCode(), diagnostics.get(0).getCode().getLeft());
     }
     
+    @Test
+    void testQuickFixAddToLoadBalancerClientConfigurationOffered() throws Exception {
+		String docUri = directory.toPath().resolve("src/main/java/com/example/cloud/loadbalancer/LoadBalancerConfigNotRegistered.java").toUri().toString();
+
+		Editor editor = harness.newEditorFromFileUri(docUri, LanguageId.JAVA);
+		Diagnostic problem = editor.assertProblem("LoadBalancerConfigNotRegistered");
+		assertEquals(Boot2JavaProblemType.MISSING_CONFIGURATION_ANNOTATION.getCode(), problem.getCode().getLeft());
+
+		List<CodeAction> codeActions = editor.getCodeActions(problem);
+		assertTrue(codeActions.size() >= 3);
+		assertTrue(codeActions.stream().anyMatch(ca -> ca.getLabel().contains("@LoadBalancerClient")));
+    }
+
+    @Test
+    void testApplyQuickFixAddToLoadBalancerClientConfiguration() throws Exception {
+		String docUri = directory.toPath().resolve("src/main/java/com/example/cloud/loadbalancer/LoadBalancerConfigNotRegistered.java").toUri().toString();
+
+		Editor editor = harness.newEditorFromFileUri(docUri, LanguageId.JAVA);
+		Diagnostic problem = editor.assertProblem("LoadBalancerConfigNotRegistered");
+		assertEquals(Boot2JavaProblemType.MISSING_CONFIGURATION_ANNOTATION.getCode(), problem.getCode().getLeft());
+
+		List<CodeAction> codeActions = editor.getCodeActions(problem);
+		CodeAction addToLbClientFix = codeActions.stream()
+				.filter(ca -> ca.getLabel().contains("@LoadBalancerClient"))
+				.findFirst()
+				.orElseThrow(() -> new AssertionError("Should have '@LoadBalancerClient' quickfix"));
+
+		Path targetFile = directory.toPath().resolve("src/main/java/com/example/cloud/loadbalancer/LoadBalancerClientExample.java");
+		String originalContent = Files.readString(targetFile);
+		try {
+			addToLbClientFix.perform();
+
+			assertEquals("""
+					package com.example.cloud.loadbalancer;
+					
+					import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+					import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+					import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+					import org.springframework.context.annotation.Bean;
+					import org.springframework.context.annotation.Configuration;
+					import org.springframework.web.reactive.function.client.WebClient;
+					
+					@Configuration
+					@LoadBalancerClients({@LoadBalancerClient(name = "first", configuration = {LoadBalancerConfigExample.class, LoadBalancerConfigNotRegistered.class})})
+					public class LoadBalancerClientExample {
+					
+					\t@Bean
+					\t@LoadBalanced
+					\tWebClient.Builder loadBalancedWebClientBuilder() {
+					\t\treturn WebClient.builder();
+					\t}
+					
+					}
+					""", Files.readString(targetFile));
+		} finally {
+			Files.writeString(targetFile, originalContent);
+		}
+    }
+
     @Test
     void testErrorAppearsWhenFeignClientAnnotationGoesAwayEntirely() throws Exception {
 		String loadBalancerClientDocUri = directory.toPath().resolve("src/main/java/com/example/cloud/loadbalancer/LoadBalancerClientExample.java").toUri().toString();

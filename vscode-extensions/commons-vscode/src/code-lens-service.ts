30aeed46c3c9c5d122f6b53fd8bd1ff65311ed39
BoykoAlex <alex.boyko@broadcom.com>
2026-03-09 16:47:46 -0700

    Fix Live Data UI in VSCode
    Signed-off-by: BoykoAlex <alex.boyko@broadcom.com>

--- vscode-extensions/commons-vscode/src/code-lens-service.ts
+++ vscode-extensions/commons-vscode/src/code-lens-service.ts
@@ -1,4 +1,4 @@
-import * as deepEqual from 'deep-equal';
+import deepEqual from 'deep-equal';
 import {
     CancellationToken,
     CodeLens,
--- vscode-extensions/commons-vscode/typings/portfinder.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'portfinder' {
-    const exports: {
-        basePort: number;
-        getPort(callback: (err: any, port: number) => void);
-    };
-    export = exports;
-}

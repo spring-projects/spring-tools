2a773a9f81e9aafd6b9f6126bb38d1e1008220a2
BoykoAlex <alex.boyko@broadcom.com>
2026-03-13 16:30:59 -0700

    Cleanup
    Signed-off-by: BoykoAlex <alex.boyko@broadcom.com>

--- headless-services/jdt-ls-extension/org.springframework.tooling.jdt.ls.commons/src/org/springframework/tooling/jdt/ls/commons/javadoc/JavadocUtils.java
+++ headless-services/jdt-ls-extension/org.springframework.tooling.jdt.ls.commons/src/org/springframework/tooling/jdt/ls/commons/javadoc/JavadocUtils.java
@@ -1,27 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2026 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.springframework.tooling.jdt.ls.commons.javadoc;
 
 import java.net.URI;
 import java.util.function.Function;
 
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.ITypeParameter;
 import org.springframework.tooling.jdt.ls.commons.java.JavaData;
 
 public class JavadocUtils {
 
 	public static final String javadoc(Function<IJavaElement, String> contentProvider, URI projectUri, String bindingKey, boolean lookInOtherProjects) throws Exception {
 		IJavaElement element = JavaData.findElement(projectUri, bindingKey, lookInOtherProjects);
-		return computeJavadoc(contentProvider, element);
-	}
-
-	private static String computeJavadoc(Function<IJavaElement, String> contentProvider, IJavaElement element) {
-		if (element == null) {
-			return null;
-		}
-		if (element instanceof ITypeParameter tp) {
-			element = tp.getDeclaringMember();
-		}
-		return contentProvider.apply(element);
+		return element == null ? null : contentProvider.apply(element);
 	}
 
 	public static String alternateBinding(String bindingKey) {

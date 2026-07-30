ad29b3276f3e6ed20d011cf85c0477357496ee48
BoykoAlex <alex.boyko@broadcom.com>
2026-07-30 12:43:09 -0700

    Cleanup
    Signed-off-by: BoykoAlex <alex.boyko@broadcom.com>

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/VSCodeBuildCommandProvider.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/VSCodeBuildCommandProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Broadcom, Inc.
+ * Copyright (c) 2025, 2026 Broadcom, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,9 +11,7 @@
 package org.springframework.ide.vscode.boot.java;
 
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.lsp4j.Command;
 import org.springframework.ide.vscode.commons.java.IJavaProject;
@@ -25,14 +23,11 @@ public class VSCodeBuildCommandProvider implements BuildCommandProvider {
 		Command cmd = new Command();
 		cmd.setCommand("maven.goal.custom");
 		cmd.setTitle("Execute Maven Goal");
-		List<Object> args = new ArrayList<>(3);
-		args.add(Paths.get(project.getProjectBuild().getBuildFile()).toFile().toString());
-		args.add(goal);
-		Map<String, String> env = BuildCommandProvider.buildEnv(project);
-		if (env != null && !env.isEmpty()) {
-			args.add(env);
-		}
-		cmd.setArguments(args);
+		cmd.setArguments(List.of(
+				Paths.get(project.getProjectBuild().getBuildFile()).toFile().toString(),
+				goal,
+				BuildCommandProvider.buildEnv(project)
+		));
 		return cmd;
 	}
 
@@ -41,14 +36,11 @@ public class VSCodeBuildCommandProvider implements BuildCommandProvider {
 		Command cmd = new Command();
 		cmd.setCommand("gradle.runBuild");
 		cmd.setTitle("Execute Gradle Build");
-		List<Object> args = new ArrayList<>(3);
-		args.add(Paths.get(project.getProjectBuild().getBuildFile()).toFile().toString());
-		args.add(command);
-		Map<String, String> env = BuildCommandProvider.buildEnv(project);
-		if (env != null && !env.isEmpty()) {
-			args.add(env);
-		}
-		cmd.setArguments(args);
+		cmd.setArguments(List.of(
+				Paths.get(project.getProjectBuild().getBuildFile()).toFile().toString(),
+				command,
+				BuildCommandProvider.buildEnv(project)
+		));
 		return cmd;
 	}
 
